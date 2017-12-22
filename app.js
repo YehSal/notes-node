af93a6e517ee2cadc565a7485b2eea2df33c4e57
Yehia Saleh <yehia.saleh@yale.edu>
2017-12-22 18:51:55 -0500

    Yargs magic

--- app.js
+++ app.js
@@ -2,5 +2,21 @@ console.log('Starting app');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require("yargs");
 
 const notes = require('./notes.js'); // Relative path
+
+const argv = yargs.argv;
+const command = argv._[0];
+
+if (command === 'add') {
+	notes.addNote(argv.title, argv.body);
+} else if (command === "list") {
+	notes.getAll();
+} else if (command === "remove") {
+	notes.removeNote(argv.title);
+} else if (command === "read" ) {
+	notes.readNote(argv.title);
+} else {
+	console.log('Command not recognized');
+}
--- notes.js
+++ notes.js
@@ -1,6 +1,24 @@
 console.log('notes.js starting!');
 
-module.exports.addNote = () => {
-	console.log('addNote');
-	return 'New note';
+var addNote = (title, body) => {
+	console.log('Adding note', title, body);
+};
+
+var getAll = () => {
+	console.log('Getting all notes');
+};
+
+var readNote = (title) => {
+	console.log("Reading", title);
+};
+
+var removeNote = (title) => {
+	console.log("Deleting", title);
+};
+
+module.exports = {
+	addNote,
+	getAll,
+	readNote,
+	removeNote
 };
