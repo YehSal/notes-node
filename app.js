8b2ec1cbc1ec49364081f8725fe614d49c6b640c
Yehia Saleh <yehia.saleh@yale.edu>
2017-12-26 18:20:33 -0500

    Refactoring

--- app.js
+++ app.js
@@ -8,13 +8,13 @@ const titleOptions = {
 	describe: 'Title of note',
 	demand: true,
 	alias: 't'				
-}
+};
 
 const bodyOptions = {
 	describe: 'Body of note',
 	demand: true,
 	alias: 'b'	
-}
+};
 
 const argv = yargs
 	.command('add', 'Add a new note', {
