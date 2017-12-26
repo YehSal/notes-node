5f31528f106e2ab2dc4f5c8af6185a31f1f07501
Yehia Saleh <yehia.saleh@yale.edu>
2017-12-26 18:08:45 -0500

    Advanced args

--- app.js
+++ app.js
@@ -4,7 +4,32 @@ const yargs = require("yargs");
 
 const notes = require('./notes.js'); // Relative path
 
-const argv = yargs.argv;
+const titleOptions = {
+	describe: 'Title of note',
+	demand: true,
+	alias: 't'				
+}
+
+const bodyOptions = {
+	describe: 'Body of note',
+	demand: true,
+	alias: 'b'	
+}
+
+const argv = yargs
+	.command('add', 'Add a new note', {
+		title: titleOptions,
+		body: bodyOptions
+	})
+	.command('list', 'List all notes')
+	.command('read', 'Read a note', {
+		title: titleOptions
+	})
+	.command('remove', 'Remove a note', {
+		title: titleOptions
+	})
+	.help()
+	.argv;
 const command = argv._[0];
 
 if (command === 'add') {
@@ -16,11 +41,15 @@ if (command === 'add') {
 		console.log('Note title taken');
 	}
 } else if (command === "list") {
-	notes.getAll();
+	var allNotes = notes.getAll();
+	console.log(`printing ${allNotes.length} note(s)`);
+	allNotes.forEach((note) => notes.logNote(note));
+
 } else if (command === "remove") {
 	var noteRemoved = notes.removeNote(argv.title);
 	var message = noteRemoved ? 'Note was removed' : 'Note not found';
 	console.log(message);
+
 } else if (command === "read" ) {
 	var note = notes.readNote(argv.title);
 	if (note) {
@@ -29,6 +58,7 @@ if (command === 'add') {
 	} else {
 		console.log("Note doesn't exist");
 	}
+
 } else {
 	console.log('Command not recognized');
 }
